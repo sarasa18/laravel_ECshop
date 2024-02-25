fb3ea53f6f72ec6e57c0a790141fd455631195b9
sarasa18 <iroenpitu122@gmail.com>
2024-02-25 23:58:49 +0900

    classBase

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,6 @@
+<div>
+    クラスベースのコンポーネント
+    <div class="">{{ $classBaseMessage }}</div>
+    <div class="">{{ $defaultMessage }}</div>
+    <!-- It is not the man who has too little, but the man who craves more, that is poor. - Seneca -->
+</div>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -3,4 +3,7 @@
     へっだー２
   </x-slot>
   コンポーネントテスト2
+  <x-test-class-base classBaseMessage="messe-jidesu" />
+  <br>
+  <x-test-class-base classBaseMessage="messe-jidesu" defaultMessage="初期値から変更" />
 </x-tests.app>
