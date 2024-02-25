568bdb75e3422c77cf5e012093b0ff1d77aea03b
sarasa18 <iroenpitu122@gmail.com>
2024-02-25 23:19:17 +0900

    props初期値の設定方法

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,6 +1,12 @@
+@props([
+  'title',
+  'message' => '初期値です。',
+  'content' => '本文初期値です。',
+  ])
+
 <div class="border-2 shadow-md w-1/4 p-2">
-<div class="">{{ $title }}</div>
-<div class="">画像</div>
-<div class="">{{ $content }}</div>
-<div class="">{{ $message }}</div>
+  <div class="">{{ $title }}</div>
+  <div class="">画像</div>
+  <div class="">{{ $content }}</div>
+  <div class="">{{ $message }}</div>
 </div>
