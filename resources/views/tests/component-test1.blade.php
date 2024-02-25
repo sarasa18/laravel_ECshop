74dafb04b54e7566e79cd7bc7c0ca7af9fa11cd6
sarasa18 <iroenpitu122@gmail.com>
2024-02-25 17:18:55 +0900

    名前付きスロット

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,4 +1,8 @@
 <x-tests.app>
+  <x-slot name="header" >
+    へっだー１
+  </x-slot>
+
   コンポーネントテスト１
 </x-tests.app>
 
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,3 +1,6 @@
 <x-tests.app>
+  <x-slot name="header" >
+    へっだー２
+  </x-slot>
   コンポーネントテスト2
 </x-tests.app>
