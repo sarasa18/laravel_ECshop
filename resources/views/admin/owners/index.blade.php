01648983f3a7ea48287c9c8afd9ef1996f76cea6
sarasa18 <iroenpitu122@gmail.com>
2024-03-15 14:58:02 +0900

    ルート、ビュー編集

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -8,9 +8,9 @@
   <div class="py-12">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-              <div class="p-6 text-gray-900 dark:text-gray-100">
+              <div class="md:p-6 text-gray-900 dark:text-gray-100">
                 <section class="text-gray-600 body-font">
-                  <div class="container px-5 mx-auto">
+                  <div class="container md:px-5 mx-auto">
                     <x-flash-message status="session('status')"/>
                     <div class="lg:w-2/3 w-full mx-auto overflow-auto">
                       <div class="flex justify-end mb-4">
--- routes/admin.php
+++ routes/admin.php
@@ -25,12 +25,12 @@ use App\Http\Controllers\Admin\OwnersController;
 |
 */
 
-Route::get('/', function () {
-    return view('admin.welcome');
-});
+// Route::get('/', function () {
+//     return view('admin.welcome');
+// });
 
 Route::resource('owners', OwnersController::class)
-->middleware(['auth:admin', 'verified']);
+->middleware(['auth:admin', 'verified'])->except(['show']);
 
 Route::prefix('expired-owners')-> middleware('auth:admin')->group(function(){
     Route::get('index', [OwnersController::class, 'expiredOwnerIndex'])->name('expired-owners.index');
