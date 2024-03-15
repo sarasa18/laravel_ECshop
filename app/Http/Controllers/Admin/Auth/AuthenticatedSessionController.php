f64b383989b0da08a9fa808f96b15cdb11746f1f
sarasa18 <iroenpitu122@gmail.com>
2024-03-16 05:56:05 +0900

    マルチログイン時の複数のセッション所持の対策

--- app/Http/Controllers/Admin/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Admin/Auth/AuthenticatedSessionController.php
@@ -9,6 +9,8 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
+use Illuminate\Support\Facades\Log;
+
 
 class AuthenticatedSessionController extends Controller
 {
@@ -28,6 +30,8 @@ class AuthenticatedSessionController extends Controller
         $request->authenticate();
 
         $request->session()->regenerate();
+
+        Log::debug('admin', $request->session()->all());
 
         return redirect()->intended(RouteServiceProvider::ADMIN_HOME);
     }
