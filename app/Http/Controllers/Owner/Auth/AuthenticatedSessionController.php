ee210156c41746e38ab24b1e9f0728a45594e6f1
sarasa18 <iroenpitu122@gmail.com>
2024-03-17 06:25:10 +0900

    編集画面の認証追記、404ページをpublicフォルダにコピペ（中身の確認）

--- app/Http/Controllers/Owner/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Owner/Auth/AuthenticatedSessionController.php
@@ -47,6 +47,6 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/owner');
+        return redirect('/owner/login');
     }
 }
--- resources/views/layouts/owner-navigation.blade.php
+++ resources/views/layouts/owner-navigation.blade.php
@@ -18,6 +18,11 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('owner.shops.index')" :active="request()->routeIs('owner.shops.index')">
+                        店舗情報
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
@@ -71,6 +76,11 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('owner.dashboard')" :active="request()->routeIs('owner.dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+        </div>
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('owner.shops.index')" :active="request()->routeIs('owner.shops.index')">
+                店舗情報
             </x-responsive-nav-link>
         </div>
 
