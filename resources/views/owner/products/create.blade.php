091197be2b85d7a371126f0fe8317284455c8c11
sarasa18 <iroenpitu122@gmail.com>
2024-03-19 10:55:10 +0900

    micromodal

--- resources/views/owner/products/create.blade.php
+++ resources/views/owner/products/create.blade.php
@@ -26,7 +26,11 @@
                           </select>
                         </div>
                       </div>
-
+                      {{-- 画像選択 --}}
+                      <x-select-image :images="$images" name="image1"  />
+                      <x-select-image :images="$images" name="image2"  />
+                      <x-select-image :images="$images" name="image3"  />
+                      <x-select-image :images="$images" name="image4"  />
 
                       <div class="p-2 w-full flex justify-around mt-4">
                         <button type="button" onclick="location.href='{{ route('owner.products.index') }}'" class="bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
@@ -38,4 +42,22 @@
           </div>
       </div>
   </div>
+  <script>
+  'use strict'
+    const images = document.querySelectorAll('.image')//全てのimageタグを取得
+    
+    images.forEach( image =>  {
+      image.addEventListener('click', function(e){
+        const imageName = e.target.dataset.id.substr(0, 6)//data-idの6文字
+        const imageId = e.target.dataset.id.replace(imageName + '_', '')// 6文字カット const imageFile = e.target.dataset.file
+        const imageFile = e.target.dataset.file
+        const imagePath = e.target.dataset.path
+        const modal = e.target.dataset.modal
+        // サムネイルと input type=hiddenのvalueに設定
+        document.getElementById(imageName + '_thumbnail').src = imagePath + '/' + imageFile
+        document.getElementById(imageName + '_hidden').value = imageId
+        MicroModal.close(modal);
+    }, )
+    })  
+  </script>
 </x-app-layout>
