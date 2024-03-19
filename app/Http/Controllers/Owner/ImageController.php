00863dd041056d6feebd465fc391a98e085d2e33
sarasa18 <iroenpitu122@gmail.com>
2024-03-19 23:16:38 +0900

    productで使っているimageを消すパターン

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
 use App\Models\Image;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Closure;
 use Illuminate\Support\Facades\Auth;
@@ -106,6 +107,34 @@ class ImageController extends Controller
     public function destroy(string $id)
     {
         $image = Image::findOrFail($id);
+
+        $imageInProducts =Product::where('image1', $image->id)
+        ->orWhere('image2', $image->id)
+        ->orWhere('image3', $image->id)
+        ->orWhere('image4', $image->id)
+        ->get();
+
+        if ($imageInProducts) {
+            $imageInProducts->each(function($product) use($image){
+                if ($product->image1 === $image->id) {
+                    $product->image1 = null;
+                    $product->save();
+                }
+                if ($product->image2 === $image->id) {
+                    $product->image2 = null;
+                    $product->save();
+                }
+                if ($product->image3 === $image->id) {
+                    $product->image3 = null;
+                    $product->save();
+                }
+                if ($product->image4 === $image->id) {
+                    $product->image4 = null;
+                    $product->save();
+                }
+            });
+        }
+
         $filePath = 'public/products/' . $image->filename;
         if (Storage::exists($filePath)) {
             Storage::delete($filePath);
