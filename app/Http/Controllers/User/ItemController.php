899e5474c3f5385bbe879164fa2a3771ed0e53ab
sarasa18 <iroenpitu122@gmail.com>
2024-03-20 00:04:38 +0900

    user.items.view調整

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use App\Models\Product;
 
 class ItemController extends Controller
 {
     public function index(){
-        
-        return view('user.index');
+
+        $products = Product::all();
+        return view('user.index', compact('products'));
     }
 }
