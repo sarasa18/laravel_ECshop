be7d3b01551ab50cb43dd296df03b584e09ede3d
sarasa18 <iroenpitu122@gmail.com>
2024-03-21 11:46:11 +0900

    show調整

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -11,6 +11,12 @@ use App\Models\Product;
 
 class ItemController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:users');
+    }
+
+
     public function index(){
         $stocks = DB::table('t_stocks')
         ->select('product_id', DB::raw('sum(quantity) as quantity'))
@@ -34,4 +40,12 @@ class ItemController extends Controller
 
         return view('user.index', compact('products'));
     }
+
+    public function show($id){
+        $product = Product::findOrFail($id);
+
+        return view('user.show', compact('product'));
+    }
+
+
 }
