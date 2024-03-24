1e33ab43f8125ee223e0653c666dda123a8d99a6
sarasa18 <iroenpitu122@gmail.com>
2024-03-25 04:17:18 +0900

    stripe controller

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use App\Models\Cart;
 use App\Models\User;
+require_once '../secrets.php';
 
 class CartController extends Controller
 {
@@ -57,5 +58,36 @@ class CartController extends Controller
         ->delete();
 
         return redirect()->route('user.cart.index');
+    }
+
+    public function checkout(){
+        $user = User::findOrFail(Auth::id());
+        $products = $user->products;
+
+        $lineItems = [];
+        foreach($products as $product){
+            $lineItem = [
+                'name' => $product->name,
+                'description' => $product->information,
+                'amount' => $product->price,
+                'currency' => 'jpy',
+                'quantity' => $product->pivot->quantity,                
+            ];
+
+            array_push($lineItems, $lineItem);
+        }
+
+        // dd($lineItem);
+        \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
+        $session = \Stripe\Checkout\Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => [[$lineItems]],
+            'mode' => 'payment',
+            'success_url' => route('user.items.index'),
+            'cancel_url' => route('user.cart.index'),
+          ]);
+
+          $publicKey = env('STRIPE_PUBLIC_KEY');
+          return view('user.checkout', compact('session', 'publicKey'));
     }
 }
