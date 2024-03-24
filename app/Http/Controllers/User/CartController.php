d16f0fbc61a4d3ede3b4a67eed9a14bcaa3af8c6
sarasa18 <iroenpitu122@gmail.com>
2024-03-25 01:07:32 +0900

    cart delete

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -48,6 +48,14 @@ class CartController extends Controller
             ]);
         }
 
-        return redirect()->route('user.cart');
+        return redirect()->route('user.cart.index');
+    }
+
+    public function delete($id){
+        Cart::where('product_id', $id)
+        ->where('user_id', Auth::id())
+        ->delete();
+
+        return redirect()->route('user.cart.index');
     }
 }
